d2819442b23e7f2c6371a209f23f7b8bdb2600d6
Mohith <ramoh563@gmail.com>
2021-01-12 17:56:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+http
+  .createServer(function (req, res) {
+    // res.write("hi");
+    // res.end();
+    const q = url.parse(req.url, true);
+    if (q.path === "/") {
+      fs.readFile("index.html", function (err, data) {
+        if (err) throw err;
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    } else if (q.path === "/about") {
+      fs.readFile("about.html", function (err, data) {
+        if (err) throw err;
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    } else if (q.path === "/contact-me") {
+      fs.readFile("contact-me.html", function (err, data) {
+        if (err) throw err;
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    } else {
+      fs.readFile("404.html", function (err, data) {
+        if (err) throw err;
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    }
+
+    console.log(q.path);
+  })
+  .listen(8080);
